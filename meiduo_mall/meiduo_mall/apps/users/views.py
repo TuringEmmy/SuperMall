7baf370443effb94434879612b29efca5832ebb2
TuringEmmy <yonglonggeng@163.com>
2018-10-25 21:46:55 +0800

    MobileCountView is ok

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -7,16 +7,36 @@ from rest_framework.views import APIView
 from users.models import User
 
 
+# ===========================判断用户名是否村存在====================================
+# GET /usernames/(?P<username>\w{5,20})/count/
 class UsernameCountView(APIView):
     """用户名数量"""
-    def get(self,request,username):
+
+    def get(self, request, username):
         """获取指定用户名数量"""
 
         count = User.objects.filter(username=username).count()
 
-        data={
-            'username':username,
-            'count':count
+        data = {
+            'username': username,
+            'count': count
+        }
+
+        return Response(data)
+
+
+# ============================判断手机号码是否存在=======================================
+# GET /mobiles/(?P<mobile>1[3-9]\d{9})/count
+class MobileCountView(APIView):
+    """手机号数量"""
+
+    def get(self, request, mobile):
+        """获取手机号数量"""
+        count = User.objects.filter(mobile=mobile).count()
+
+        data = {
+            'mobile': mobile,
+            'count': count,
         }
 
         return Response(data)
