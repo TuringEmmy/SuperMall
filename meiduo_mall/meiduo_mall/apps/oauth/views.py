26c02c6646d57b331ae5d342a5abc95c34dd392e
TuringEmmy <yonglonggeng@163.com>
2018-10-28 17:18:16 +0800

    QQ登陆获取QQ登陆后端的网址

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from oauth.utils import OAuthQQ
 
+
+# GET /oauth/qq/authorization/?next=<登陆跳转地址>
 class QQAuthURLView(APIView):
     """返回qq认证接口的url"""
     """
@@ -12,7 +16,22 @@ class QQAuthURLView(APIView):
     2. 请求方式  GET
     3. 请求参数
     4. 返回相应  json(qq认证接口的认证)
-    """
-    def get(self, request):
 
-        pass
+    获取QQ登陆网址
+    1. 获取next
+    2. 组织QQ登陆的网址参数
+    3. 返回QQ登陆的网站
+    """
+
+    def get(self, request):
+        # 1.获取next
+        next = request.query_params.get('next', '/')
+
+        # 2.组织QQ登陆的网址参数
+        oauth = OAuthQQ(state=next)
+
+        login_url = oauth.get_login_url()
+
+        # 3.返回QQ登陆的网站
+        return Response({"login_url": login_url})
+
