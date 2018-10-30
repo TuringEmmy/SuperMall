c34943a4c3dcf7ac7b2cdff22a68d1fa390e6de3
TuringEmmy <yonglonggeng@163.com>
2018-10-30 20:59:33 +0800

    provinces is ok

--- meiduo_mall/meiduo_mall/apps/areas/serializers.py
+++ meiduo_mall/meiduo_mall/apps/areas/serializers.py
@@ -7,6 +7,8 @@ from rest_framework import serializers
 from areas.models import Area
 
 
+
+# 获取省的序列化
 class AreaSerializer(serializers.ModelSerializer):
     """
     地区序列化器类
@@ -15,3 +17,19 @@ class AreaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Area
         fields = ('id', 'name')
+
+
+
+# 获取市和县的序列化
+
+class SubAreaSeializer(serializers.ModelSerializer):
+
+    # 使用序列化:使用指定的序列化
+    subs = AreaSerializer(label='下级地区',many=True)
+    class Meta:
+        model = Area
+        fields =('id','name','subs')
+
+"""
+注意:这里使用到了嵌套序列化,这个方法甚是巧妙
+"""
