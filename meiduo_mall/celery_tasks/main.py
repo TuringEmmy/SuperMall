00d8fd60dbdf6f4b6246aa79be50ae5240867c81
TuringEmmy <yonglonggeng@163.com>
2018-10-30 18:13:37 +0800

    celery send email is ok

--- meiduo_mall/celery_tasks/main.py
+++ meiduo_mall/celery_tasks/main.py
@@ -1,4 +1,12 @@
+import os
+
 from celery import Celery
+
+
+
+# 设置django所运行的环境
+if not os.environ.get("DJANGO_SETTINGS_MODULE"):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meiduo_mall.settings")
 
 # 创建Celery类的实例对象
 celery_app = Celery('celery_tasks')
@@ -7,4 +15,6 @@ celery_app = Celery('celery_tasks')
 celery_app.config_from_object('celery_tasks.config')
 
 # 启动celery worker时自动发现任务
-celery_app.autodiscover_tasks(['celery_tasks.sms'])
+celery_app.autodiscover_tasks(['celery_tasks.sms','celery_task.email'])
+
+# 注意:这里有一个大坑,进行邮箱的发送的时候,使用到了setting,因此,这个main的模块需要进行django的环境设置
--- meiduo_mall/meiduo_mall/apps/users/serializers.py
+++ meiduo_mall/meiduo_mall/apps/users/serializers.py
@@ -47,6 +47,10 @@ class EmailSerializer(serializers.ModelSerializer):
         #            '<p><a href="%s">%s<a></p>' % (email, verify_url, verify_url)
         # send_mail(subject, "", settings.EMAIL_FROM, [email], html_message=html_message)
 
+        # =========================启用多进程========================
+        from celery_tasks.email.tasks import send_verify_email
+        send_verify_email(email, verify_url)
+        # =========================启用多进程========================
 
         # 返回instance
         return instance
