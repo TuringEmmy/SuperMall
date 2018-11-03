6e0bc1c13c8bd162641de540daf2e2b670b7d5f8
TuringEmmy <yonglonggeng@163.com>
2018-11-03 20:37:00 +0800

    update detail page when model have edit <obj.save()>

--- meiduo_mall/meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/meiduo_mall/apps/goods/admin.py
@@ -1,8 +1,52 @@
-from django.contrib import admin
 
 # Register your models here.
 from goods.models import GoodsCategory, GoodsChannel, Goods, Brand, GoodsSpecification, SpecificationOption, SKU, \
     SKUSpecification, SKUImage
+
+from django.contrib import admin
+
+# Register your models here.
+
+
+
+class SKUAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(obj.id)
+
+
+class SKUSpecificationAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(obj.sku.id)
+
+    def delete_model(self, request, obj):
+        sku_id = obj.sku.id
+        obj.delete()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(sku_id)
+
+
+class SKUImageAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(obj.sku.id)
+
+        # 设置SKU默认图片
+        sku = obj.sku
+        if not sku.default_image_url:
+            sku.default_image_url = obj.image.url
+            sku.save()
+
+    def delete_model(self, request, obj):
+        sku_id = obj.sku.id
+        obj.delete()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(sku_id)
+
 
 admin.site.register(GoodsCategory)
 admin.site.register(GoodsChannel)
@@ -10,6 +54,7 @@ admin.site.register(Goods)
 admin.site.register(Brand)
 admin.site.register(GoodsSpecification)
 admin.site.register(SpecificationOption)
-admin.site.register(SKU)
-admin.site.register(SKUSpecification)
-admin.site.register(SKUImage)
+admin.site.register(SKU, SKUAdmin)
+admin.site.register(SKUSpecification, SKUSpecificationAdmin)
+admin.site.register(SKUImage, SKUImageAdmin)
+
